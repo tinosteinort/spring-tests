3727ff6809f0af9d414fcc85af516d537c9dd3f2
Christopher Klein <me@schakko.de>
2016-07-27 09:19:18 +0200

    Fixed usage of custom factory bean
    Please read JavaDoc comment in NotWorkingFactoryBeanConfig for further explainations.

--- src/main/java/de/tse/spring/factorybeantest/NotWorkingFactoryBeanConfig.java
+++ src/main/java/de/tse/spring/factorybeantest/NotWorkingFactoryBeanConfig.java
@@ -8,16 +8,46 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NotWorkingFactoryBeanConfig {
 
-    @Bean public PrintService printService() throws Exception {
-        return new PrintService(decorator());
-    }
+	@Bean
+	public PrintService printService(Decorator decorator) throws Exception {
+		return new PrintService(decorator);
+	}
 
-    @Bean public DecoratorFromAbstractFactoryBean decoratorFromAbstractFactoryBean() {
-        return new DecoratorFromAbstractFactoryBean();
-    }
+	@Bean
+	public DecoratorFromAbstractFactoryBean decoratorFromAbstractFactoryBean() {
+		DecoratorFromAbstractFactoryBean r = new DecoratorFromAbstractFactoryBean();
+		return r;
+	}
 
-    @Bean public Decorator decorator() throws Exception {
-        //  If the DecoratorFactoryBean extends AbstractFactoryBean, the Decorator Interface can not be loaded by the ClassLoader
-        return decoratorFromAbstractFactoryBean().getObject();
-    }
+	/**
+	 * CKL
+	 * <ul>
+	 * <li>Tried to upgrade to 4.2.4-BUILD-SNAPSHOT
+	 * (https://jira.spring.io/browse/SPR-13696) but the error still occurred.
+	 * </li>
+	 * <li>Saw, that call to
+	 * <em>decoratorFromAbstractFactoryBean()<--.getObject()</em> has been made.
+	 * This will <strong>not</strong> return a Spring-managed factory bean.
+	 * Because of this <em>AbstractFactoryBean::getEarlySingletonInstance {
+	 * Proxy.newProxyInstance(...) }</em> failed. The classLoader parameter for
+	 * <em>newInstance</em> has been null.as the class loader has no access to
+	 * the de.tse package. I am not sure why the error directly happens when not
+	 * using Spring instantiation. From my understanding newProxyInstance does
+	 * allow null values and should fall back to the default class loader. There
+	 * could be some problems with CGLIB but I am not sure with that.</li>
+	 * <li>Error occurred in the first place by misusing Spring's mechanism.
+	 * </li>
+	 * </ul>
+	 * 
+	 * @param decoratorFromAbstractFactoryBean
+	 *            factory bean must be manually injected so that
+	 *            afterPropertiesSet() is called by Spring
+	 * @return
+	 * @throws Exception
+	 */
+	@Bean
+	public Decorator decorator(DecoratorFromAbstractFactoryBean decoratorFromAbstractFactoryBean) throws Exception {
+		// FIX
+		return decoratorFromAbstractFactoryBean/* Spring managed bean */.getObject();
+	}
 }
